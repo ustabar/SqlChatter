3199f8d07ab2583e3c82181255f189246782ec91
Baris Usta <barisu@microsoft.com>
2024-05-31 10:10:11 +0300

    chore: Update OpenAIWebApp assembly version to 1.0.0+b206497fdf6324c85fa7860342facfbb1a18d25c

--- obj/Debug/net8.0/OpenAIWebApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/OpenAIWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OpenAIWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17c7a28bbd52cb03573c04d0650cb0e860fcdc71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b206497fdf6324c85fa7860342facfbb1a18d25c")]
 [assembly: System.Reflection.AssemblyProductAttribute("OpenAIWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OpenAIWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
